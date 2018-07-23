c3d7ef71e0248765ddd44ea1c003fd1085b0c4da
papb <papb1996@gmail.com>
2018-07-23 18:48:33 -0300

    Add array-to-picker-function.js

---
+++ src/array-to-picker-function.js
@@ -0,0 +1,62 @@
+/**
+ * Creates a function that yields the elements of the given array in order when called with
+ * different arguments (and is idempotent). If the end of the array is reached, picking will
+ * restart from the beginning. The behaviour of this function is better understood by example
+ * (which can be found below).
+ * 
+ * @param {Array} array The source array
+ * @return {function} The picker function
+ * 
+ * @example
+ * 
+ * ```
+ * let f = arrayToPickerFunction([1, 2, 3]);
+ * let w = 55;
+ * let x = {};
+ * let y = {};
+ * let z = "something";
+ * f(w); // 1
+ * f(x); // 2
+ * f(w); // 1
+ * f(y); // 3
+ * f(z); // 1
+ * f(y); // 3
+ * f(y); // 3
+ * f(x); // 2
+ * f(z); // 1
+ * ```
+ */
+let arrayToPickerFunction = function(array) {
+    if (!Array.isArray(array)) throw new TypeError("Parameter must be an array.");
+    let localArray = array.slice(); // If the given array changes, this won't change.
+    let indexMap = new Map();
+    let nextIndex = 0;
+    return function(obj) {
+        if (!indexMap.has(obj)) {
+            indexMap.set(obj, nextIndex);
+            nextIndex = (nextIndex + 1) % localArray.length;
+        }
+        return localArray[indexMap.get(obj)];
+    };
+};
+
+/* test */ module.exports = {
+    snippet: arrayToPickerFunction,
+    snippetName: "arrayToPickerFunction",
+    snippetTest: t => {
+        let f = arrayToPickerFunction([1, 2, 3]);
+        let w = 55;
+        let x = {};
+        let y = {};
+        let z = "something";
+        t.is(f(w), 1);
+        t.is(f(x), 2);
+        t.is(f(w), 1);
+        t.is(f(y), 3);
+        t.is(f(z), 1);
+        t.is(f(y), 3);
+        t.is(f(y), 3);
+        t.is(f(x), 2);
+        t.is(f(z), 1);
+    }
+};
