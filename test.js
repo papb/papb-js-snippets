c910a0e569efff3594d478049bb089917f17bb9d
papb <papb1996@gmail.com>
2018-07-24 19:45:56 -0300

    Build tests synchronously

--- test.js
+++ test.js
@@ -1,21 +1,19 @@
 const test = require('ava');
-const Promise = require('bluebird');
-const fs = Promise.promisifyAll(require('fs'));
+const fs = require('fs');
 const path = require('path');
 
-fs.readdirAsync("src").then(files => {
-    files = files.map(file => ({
-        filename: file,
-        content: require(path.join(__dirname, "src", file))
-    }));
+const files = fs.readdirSync("src").map(file => ({
+    filename: file,
+    content: require(path.join(__dirname, "src", file))
+}));
 
-    global.snippets = {};
-    files.forEach(file => {
-        global.snippets[file.content.snippetName] = file.content.snippet;
-    });
+global.snippets = {};
 
-    files.forEach(file => {
-        const name = `${file.content.snippetName} (${file.filename})`;
-        test(name, file.content.snippetTest);
-    });
+files.forEach(file => {
+    global.snippets[file.content.snippetName] = file.content.snippet;
+});
+
+files.forEach(file => {
+    const name = `${file.content.snippetName} (${file.filename})`;
+    test(name, file.content.snippetTest);
 });
